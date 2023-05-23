89537d562651301fa78ff4551e185ce8576f8077
HaroldF415 <harold.f415@gmail.com>
2023-05-23 13:17:57 -0400

    Update getUserById function to handle no data case"

--- queries/userQueries.js
+++ queries/userQueries.js
@@ -1,9 +1,9 @@
 async function getUserById(id) {
   try {
     const user = await db.one("SELECT * FROM users WHERE id = $1", id);
-    const achievements = await db.many("SELECT * FROM achievements WHERE user_id = $1", id);
-    const allergies = await db.many("SELECT * FROM allergies WHERE user_id = $1", id);
-    const medications = await db.many("SELECT * FROM medications WHERE user_id = $1", id);
+    const achievements = await db.manyOrNone("SELECT * FROM achievements WHERE user_id = $1", id);
+    const allergies = await db.manyOrNone("SELECT * FROM allergies WHERE user_id = $1", id);
+    const medications = await db.manyOrNone("SELECT * FROM medications WHERE user_id = $1", id);
 
     user.achievements = achievements;
     user.allergies = allergies;
