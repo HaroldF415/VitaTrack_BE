d8227f2e18e3fc0ebd14c32ba45bbe723ac792a4
HaroldF415 <harold.f415@gmail.com>
2023-05-23 12:45:37 -0400

    feat: implement user routes and controller in userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,13 +6,53 @@ const validateUser = require("../validations/userValidations.js");
 
 const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require("../queries/userQueries.js");
 
-// INDEX ROUTE
-userRouter.get("/", async (req, res) => {
-  const users = await getAllUsers();
-
-  if (!users[0]) {
+// Helper Error Function
+const handleErrors = (res, error, data) => {
+  if (error?.code === 0) {
+    return res.status(404).json({ error: "User Not Found" });
+  } else if (error) {
     return res.status(500).json({ error: "Server Error" });
   }
 
-  res.status(200).json(users);
+  return res.status(200).json(data);
+};
+
+// INDEX ROUTE
+userRouter.get("/users", async (req, res) => {
+  const { error, users } = await getAllUsers();
+
+  return handleErrors(res, error, users);
 });
+
+// SHOW USER ROUTE - by id
+userRouter.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { error, user } = await getUserById(id);
+
+  return handleErrors(res, error, user);
+});
+
+// ADD USER ROUTE
+userRouter.post("/users", validateUser, async (req, res) => {
+  const { error, newUser } = await createUser(req.body);
+
+  return handleErrors(res, error, newUser);
+});
+
+// UPDATE USER ROUTE
+userRouter.put("/users/:id", validateUser, async (req, res) => {
+  const { id } = req.params;
+  const { error, updatedUser } = await updateUser(id, req.body);
+
+  return handleErrors(res, error, updatedUser);
+});
+
+// DELETE USER ROUTE
+userRouter.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { error, deletedUser } = await deleteUser(id);
+
+  return handleErrors(res, error, deletedUser);
+});
+
+module.exports = userRouter;
