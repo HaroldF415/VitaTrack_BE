d7ea003b5f17eaaedb998b5b6b0ab4c3a24450bb
HaroldF415 <harold.f415@gmail.com>
2023-05-23 12:44:31 -0400

    feat: implement createUserValidator middleware for user data validation

---
+++ validations/createUserValidator.js
@@ -0,0 +1,10 @@
+const createUserValidator = (schema) => (req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next();
+};
+
+module.exports = createUserValidator;
